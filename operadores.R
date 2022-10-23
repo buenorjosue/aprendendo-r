db38f7536d30afecec1f252fab14bc6e2db98af3
josuebuenor <josuebuenor@ufrrj.br>
2022-10-23 18:55:43 -0300

    Entendendo os operadores em R

---
+++ operadores.R
@@ -0,0 +1,51 @@
+# Soma
+5 + 5
+
+# Subtração
+10 - 8
+
+# Divisão 
+9 / 3
+
+# Multiplicação
+4 * 2
+
+# Potência
+4 ** 2
+4 ^ 2
+
+# Igual?
+5 == 6
+5 == 5
+
+# Diferente?
+5 != 6
+5 != 5
+
+# Maior?
+4 > 2
+5 > 10
+
+# Menor?
+4 < 2
+5 < 10 
+
+# Maior ou igual?
+4 >= 5
+4 >= 4
+
+# Menor ou igual?
+4 <= 5
+4 <= 4
+
+# E
+6 == 6
+6 == 6 & 7 == 8
+
+# OU
+6 == 6
+6 == 6 | 7 == 8
+
+# Negação
+!6 == 8
+!6 != 6
